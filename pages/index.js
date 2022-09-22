c4c4fd7c0ff8e721d12a74ca497f86bd2113f235
TalDmitrii <30574780+TalDmitrii@users.noreply.github.com>
2022-09-22 16:58:19 +0300

    Getting data from mongo

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,30 @@
 import MeetupList from "../components/meetups/MeetupList";
-
-const DUMMY_MEETUPS = [
-    {
-        id: "m1",
-        title: "A first meetup",
-        image: "https://wallpapercave.com/wp/5d5Ox2o.jpg",
-        address: "Some city",
-        description: "Gorgeous place...",
-    },
-    {
-        id: "m2",
-        title: "A second meetup",
-        image: "https://wallpapercave.com/wp/s40TMOk.jpg",
-        address: "Some second city",
-        description: "Gorgeous place...",
-    },
-];
+import { MongoClient } from "mongodb";
 
 function HomePage(props) {
     return <MeetupList meetups={props.meetups} />;
 }
 
 export async function getStaticProps() {
+    const client = await MongoClient.connect(
+        "mongodb+srv://react-nextjs:react-nextjs@cluster0.tlgrilq.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+    const meetupsCollection = db.collection("meetups");
+    const meetups = await meetupsCollection.find().toArray();
+
+    client.close();
+
     return {
         props: {
-            meetups: DUMMY_MEETUPS,
+            meetups: meetups.map((meetup) => ({
+                id: meetup._id.toString(),
+                title: meetup.title,
+                image: meetup.image,
+                address: meetup.address,
+                // description: meetup.description,
+            })),
         },
         revalidate: 10,
     };
